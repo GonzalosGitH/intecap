92fc3d548483a8e58c83c65a7d527fd54694266e
GonzalosGitH <lacangonzalo@gmail.com>
2024-10-01 23:13:37 -0600

    AH3 se realizo la practica para consumir el servicio de productos utilizando lenguaje java con springboot

--- src/main/java/com/example/intecap/models/productosModel.java
+++ src/main/java/com/example/intecap/models/productosModel.java
@@ -5,26 +5,31 @@ import java.io.Serializable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.GenerationType;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-@Table(name= "productos")
 
+@Table(name = "productos")
 public class productosModel implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     @Column(name = "id")
     private int id;
 
     @Column(name = "nombre")
     private String nombre;
 
-    @Column(name = "apellido")
-    private String apellido;
+    @Column(name = "precio")
+    private double precio;
+
+    @Column(name = "stock")
+    private int stock;
 }
--- src/main/java/com/example/intecap/repository/clienteRepository.java
+++ src/main/java/com/example/intecap/repository/clienteRepository.java
@@ -2,9 +2,9 @@ package com.example.intecap.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import com.example.intecap.models.clientesModel;
+import com.example.intecap.models.productosModel;
 
-public interface clienteRepository extends CrudRepository<clientesModel, Object>{
+public interface clienteRepository extends CrudRepository<productosModel, Object>{
 
 
     
